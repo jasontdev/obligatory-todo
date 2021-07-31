4d291d49c4f7bace4e5339014bc998c629895368
Jason Telfer <jason@jasont.dev>
2021-07-31 10:24:56 +1000

    Basic login UI complete. Ready to be hooked into Firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <div className="container">
+        <Header className="header"></Header>
+        <LoginPage className="login-page"></LoginPage>
+      </div>
+    </div>
+  );
+}
+
+function Header(props) {
+  return (
+    <div className="header">
+      <h1>Obligatory Todo</h1>
+    </div>
+  );
+}
+
+function LoginPage(props) {
+  return (
+    <div class="login-page">
+      <LoginBox></LoginBox>
+    </div>
+  );
+}
+
+function LoginBox(props) {
+  return (
+    <div className="login-box">
+      <div className="login-box-row">
+        <label for="username" className="login-box-label">
+          <FontAwesomeIcon icon={ faUser } />
+        </label>
+        <input id="username" type="email" 
+        className="text-input-box"
+        placeholder="Username"></input>
+      </div>
+      <div className="login-box-row">
+        <label for="password" className="login-box-label">
+          <FontAwesomeIcon icon={faKey} />
+        </label>
+        <input id="password" type="password" 
+        className="text-input-box" 
+        placeholder="Password"></input>
+      </div>
+      <div className="login-box-button-row">
+        <button className="login-button">Login</button>
+      </div>
     </div>
   );
 }
