347875819d6e012133004e322aca499f31e09f67
Jason Telfer <jason@jasont.dev>
2021-08-03 06:29:35 +1000

    Blanked project. Need to start again.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,10 @@
 import './App.css';
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faKey } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
     <div class="App">
-      <div className="container">
-        <Header className="header"></Header>
-        <LoginPage className="login-page"></LoginPage>
-      </div>
-    </div>
-  );
-}
-
-function Header(props) {
-  return (
-    <div className="header">
-      <h1>Obligatory Todo</h1>
-    </div>
-  );
-}
-
-function LoginPage(props) {
-  return (
-    <div class="login-page">
-      <LoginBox></LoginBox>
-    </div>
-  );
-}
-
-function LoginBox(props) {
-  return (
-    <div className="login-box">
-      <div className="login-box-row">
-        <label for="username" className="login-box-label">
-          <FontAwesomeIcon icon={ faUser } />
-        </label>
-        <input id="username" type="email" 
-        className="text-input-box"
-        placeholder="Username"></input>
-      </div>
-      <div className="login-box-row">
-        <label for="password" className="login-box-label">
-          <FontAwesomeIcon icon={faKey} />
-        </label>
-        <input id="password" type="password" 
-        className="text-input-box" 
-        placeholder="Password"></input>
-      </div>
-      <div className="login-box-button-row">
-        <button className="login-button">Login</button>
-      </div>
-    </div>
+      <h1>Hello, world</h1>
+    </div >
   );
 }
 
