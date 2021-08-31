d787f5d9ae1f7996064a8077191e3d5289f3669e
Jason Telfer <jason@jasont.dev>
2021-08-31 18:04:41 +1000

    Register redirects to Todos rather than Login. Utilises auth state observer

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -1,20 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import firebase from "firebase/app";
 import {Redirect} from "react-router-dom";
 
 function RegisterPage(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [routeToLogin, setRouteToLogin] = useState(false);
+  const [routeToTodos, setRouteToTodos] = useState(false);
 
   // TODO handle success/fail of registration. Show error or redirect to login
   const onButtonClick = function registerUserWithFirebase() {
-    firebase.auth().createUserWithEmailAndPassword(username, password)
-      .then(() => setRouteToLogin(true));
+    firebase.auth().createUserWithEmailAndPassword(username, password);
   }
-  
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        setRouteToTodos(true);
+      }
+    })
+  })
   return (
-    routeToLogin ? <Redirect to="/login" /> :
+    routeToTodos ? <Redirect to="/login" /> :
     <div className="flex-column">
       <h2>Register</h2>
       <input id="username" value={username} className="text-input flex-column-item"
